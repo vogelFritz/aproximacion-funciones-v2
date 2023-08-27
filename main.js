c224487f38da9f0fdb13c2d32250ffd3e3dce732
Francisco Santin <franciscosantin2002@gmail.com>
2023-08-27 11:46:57 -0300

    Métodos agregados

--- main.js
+++ main.js
@@ -4,8 +4,14 @@ import { Grafico, Punto, Linea } from './src/grafico/grafico';
 const grafico = new Grafico();
 
 // TESTEO
+const pto1 = new Punto(500, 500),
+      pto2 = new Punto(700, 700);
 
-grafico.agregarPunto( new Punto( 500, 500 ) )
+grafico.agregarPunto( pto1 );
+grafico.agregarPunto( pto2 );
+grafico.agregarPunto( new Punto(800, 1200) );
+grafico.agregarLinea( new Linea( pto1, pto2 ) );
+grafico.graficarPolinomio( grafico.obtenerCoefMinimosCuadrados(5) );
 
 const funcion1 = ( x ) => 2 * x;
 
--- src/grafico/grafico.js
+++ src/grafico/grafico.js
@@ -1,11 +1,16 @@
 import { agregarZoom, volverArrastrable } from './movimiento';
+import { lagrange, polinomio } from '../metodos/lagrange';
+import { minimosCuadrados } from '../metodos/minimos_cuadrados';
+import { splinesCubicos } from '../metodos/splines_cubicos';
 
 // CLASES
 
 export class Grafico {
 	divHTML;
     ventanaHTML;
-	puntos = [];
+	puntos  = [];
+	puntosX = [];
+	puntosY = [];
 
 	constructor() {
 		this.divHTML = document.querySelector('.grafico');
@@ -18,9 +23,12 @@ export class Grafico {
 		punto.divHTML.style.left = `${ (punto.x / this.divHTML.offsetWidth ) * 100 }%`;
 		this.divHTML.append( punto.divHTML );
 		this.puntos.push( punto );
+		this.puntosX.push( punto.x );
+		this.puntosY.push( punto.y );
 	}
-	agregarLinea() {
-
+	agregarLinea( linea ) {
+		linea.divHTML.style.width = `${ (linea.largo / this.divHTML.offsetWidth) * 100 }%`;
+		this.divHTML.append( linea.divHTML );
 	}
 	graficarFuncion( f ) {
 		const h = 5.;
@@ -35,6 +43,22 @@ export class Grafico {
 	borrarTodo() {
 		this.divHTML.replaceChildren('');
 	}
+	obtenerCoefLagrange() { 
+		return lagrange( this.puntosX, this.puntosY ); 
+	}
+	obtenerCoefMinimosCuadrados( numTerminos ) {
+		return minimosCuadrados( this.puntosX, this.puntosY, numTerminos );
+	}
+	obtenerCoefSplinesCubicos() {
+		return splinesCubicos( this.puntosX, this.puntosY );
+	}
+	graficarPolinomio( coef ) {
+		this.graficarFuncion( ( x ) => polinomio(x, coef) );
+	}
+	graficarSplinesCubicos() {
+		
+	}
+	
 }
 
 export class Punto {
@@ -50,7 +74,22 @@ export class Punto {
 }
 
 export class Linea {
-
+	pto1;
+	pto2;
+	largo;
+	divHTML;
+	constructor( pto1, pto2 ) {
+		const deltaY = pto2.y - pto1.y,
+			  deltaX = pto2.x - pto1.x;
+		this.pto1 = pto1;
+		this.pto2 = pto2;
+		this.divHTML = document.createElement('div');
+		this.divHTML.className = 'linea';
+		this.largo = Math.sqrt( deltaX ** 2. + deltaY ** 2. );
+		this.divHTML.style.transform = `rotate(${ Math.atan2( deltaY, deltaX ) }rad)`;
+		this.divHTML.style.top  = pto1.divHTML.style.top;
+		this.divHTML.style.left = pto1.divHTML.style.left;
+	}
 }
 
 // FIN CLASES
